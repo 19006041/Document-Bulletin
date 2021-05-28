665c7c5faf25f3edf117b10ca8c4b7e172a86f9e
Blake <19006041@vcconnect.co.za>
2021-05-28 10:31:44 +0200

    register validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -17,16 +18,21 @@ export class RegisterComponent implements OnInit {
   }
 
 
-registerUser(){
-this._auth.registerUser(this.registerUserData)
-.subscribe(
-  res => {
-    console.log(res)
-    localStorage.setItem('token', res.token)
-    this._router.navigate(['/bulletin'])
-  },
-  err => console.log(err)
-)
+registerUser(regForm:NgForm){
+
+  if(regForm.valid){
+    this._auth.registerUser(this.registerUserData)
+    .subscribe(
+      res => {
+        console.log(res)
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/bulletin'])
+      },
+      err => console.log(err)
+    )
+  }
+
+
 }
 
 }
