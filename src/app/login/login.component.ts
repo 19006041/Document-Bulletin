7b1fbe53cd94e6361e89e2edfe3a1d01105b5be3
Blake <19006041@vcconnect.co.za>
2021-05-28 10:03:27 +0200

    loginvalidation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -17,16 +18,20 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-loginUser(){
-  this._auth.loginUser(this.loginUserData)
-  .subscribe(
-    res => {
-      console.log(res)
-      localStorage.setItem('token', res.token)
-      this._router.navigate(['/bulletin'])
-    },
-    err => console.log(err)
-  )
+loginUser(logForm:NgForm){
+
+  if(logForm.valid){
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        console.log(res)
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/bulletin'])
+      },
+      err => console.log(err)
+    )
+  }
+
 }
 
 }
