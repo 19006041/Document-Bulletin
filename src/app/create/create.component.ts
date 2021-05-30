550b8712729c0a768df957080fecd2d7607e06c9
Blake <19006041@vcconnect.co.za>
2021-05-30 20:21:57 +0200

    outputs order kinda

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { BulletinService } from '../bulletin.service';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -13,16 +15,38 @@ export class CreateComponent implements OnInit {
   requiredDetailsError = 'Details are required';
   invalidDetailsError = 'Detail text is invalid';
 
+  documents = []
   createDocumentData:any = {}
-  constructor(
+  constructor(private _bulletinService: BulletinService,
     private _router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+      this._bulletinService.getBulletin()
+      .subscribe(
+        res => this.documents = res,
+        err => {
+          if (err instanceof HttpErrorResponse){
+            if (err.status == 401){
+              this._router.navigate(['/login'])
+            }
+          }
+        }
+      )
+    }
+
 
   createDocument(logForm:NgForm){
 
 
+    const doc = {
+      _id: "40",
+      name: this.createDocumentData.title,
+      description: "lorem ipsum",
+      date: "2012-04-23T18:25:43.511Z",
+   }
+
+    this.documents.push(doc)
+    console.log(this.documents[6].name)
 
   }
 
